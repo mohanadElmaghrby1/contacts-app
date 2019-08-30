7eb799457cca73e4fd6bd3dcb6a6e9925d704176
mohanadElmaghrby1 <mohanad2021996@gmail.com>
2019-08-30 21:23:44 +0200

    add redirect to home page after sign up

--- src/main/java/com/mohannad/contactsapp/controllers/UserController.java
+++ src/main/java/com/mohannad/contactsapp/controllers/UserController.java
@@ -29,6 +29,11 @@ public class UserController {
         return "user/index";
     }
 
+    @RequestMapping("/home")
+    public String homePage(Model model){
+        return "index";
+    }
+
     /*handle the post back from /recipe/new */
     @PostMapping //    @RequestMapping(name = "recipe" , method = RequestMethod.POST) is the same
     @RequestMapping("user")
@@ -36,9 +41,8 @@ public class UserController {
         //save the returned user command to db
         UserCommand saveUserCommand = userService.saveUserCommand(userCommand);
         System.out.println("user saved successfully "+saveUserCommand);
-
         //redirect to the recipe show
-        return "index";
+        return "redirect:/home";
     }
 
 }
