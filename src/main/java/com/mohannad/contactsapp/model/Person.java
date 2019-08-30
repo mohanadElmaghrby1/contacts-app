004efaecf672bd4840efb00699f83f4dad11e711
mohanadElmaghrby1 <mohanad2021996@gmail.com>
2019-08-30 10:03:32 +0200

    create Contact Entity and User Entity

---
+++ src/main/java/com/mohannad/contactsapp/model/Person.java
@@ -0,0 +1,69 @@
+package com.mohannad.contactsapp.model;
+
+import javax.persistence.Column;
+import javax.persistence.Lob;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.UniqueConstraint;
+
+/**
+ * created by mohannad
+ */
+@MappedSuperclass // indicates that
+public class Person extends BaseEntity {
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName ;
+
+    @Column(name = "phone", unique = true)
+    private String phone;
+
+    @Column(name = "email" , unique = true)
+    private String email;
+
+    @Lob
+    private Byte [] image;
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Byte[] getImage() {
+        return image;
+    }
+
+    public void setImage(Byte[] image) {
+        this.image = image;
+    }
+}
